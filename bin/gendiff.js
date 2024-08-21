91a2a1b1f2401807ab6eb9cf732296db7b81fcdf
Денис Вершков <valeriyabusy@gmail.com>
2024-08-21 17:07:48 +0300

    add JSON file1 and file2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { fs } from 'node:fs';
+import { cwd } from 'node:process';
+import { path } from 'node:path';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .argument('<filePath1>', 'path for the first file')
+  .argument('<filePath2>', 'path for the second file')
+  .action;
 
 program.parse();
 
